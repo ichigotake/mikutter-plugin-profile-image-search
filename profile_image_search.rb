2b8eb8ee6045dcfc26853bd428402f972e34761f
Takayuki Otake <k.wisiiy@gmail.com>
2013-05-19 10:42:15 +0900

    二次元画像詳細検索に対応
    http://www.ascii2d.net/

--- profile_image_search.rb
+++ profile_image_search.rb
@@ -3,6 +3,7 @@
 require 'net/http'
 require 'uri'
 require 'mechanize'
+require 'nokogiri'
 
 Plugin.create(:profile_image_search) do
 
@@ -12,8 +13,24 @@ Plugin.create(:profile_image_search) do
         return agent.page.uri.to_s
     end
 
+    def _search_ascii2d(msg)
+        agent = Mechanize.new;
+        agent.user_agent = 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)';
+        
+        search = agent.get('http://www.ascii2d.net/imagesearch')
+        result = search.form_with(:name => 'webform') do |form|
+            image_url = get_profile_image_url(msg)
+            form.uri = image_url;
+        end.submit
 
-  command(:profile_image_search,
+        hash = result.root.search('.detailbox .md5')[0].text
+        
+        return 'http://www.ascii2d.net/imagesearch/similar/' + hash
+    end
+
+
+  #Google画像検索
+  command(:profile_image_search_google,
             name: 'アイコンを画像検索する(Google画像検索)',
             condition: Plugin::Command[:HasOneMessage],
             visible: true,
@@ -21,6 +38,18 @@ Plugin.create(:profile_image_search) do
                 m.messages.map do |msg|
                     image_url = get_profile_image_url(msg)
                     Gtk::openurl("https://images.google.com/searchbyimage?image_url=#{image_url}")
+                end
+            end
+
+  #二次元画像詳細検索
+  command(:profile_image_search_acsii2d,
+            name: 'アイコンを画像検索する(二次元詳細画像検索)',
+            condition: Plugin::Command[:HasOneMessage],
+            visible: true,
+            role: :timeline) do |m|
+                m.messages.map do |msg|
+                    url = _search_ascii2d(msg)
+                    Gtk::openurl(url)
                 end
             end
 
